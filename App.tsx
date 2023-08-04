88d306f241dc31e478415a9a1293c23eb85a0af1
Nizam <ramazanov570633@gmail.com>
2023-08-04 21:27:10 +0400

    End Ui

--- App.tsx
+++ App.tsx
@@ -14,10 +14,10 @@ const App = () => {
     <NavigationContainer> 
       <Provider store={store}>
       <Stack.Navigator screenOptions={{headerShown:false}}>
-      {/* <Stack.Screen name="OnboardingScreen" component={OnboardingScreen}/>
-      <Stack.Screen name="LoginRegisterScreen" component={LoginRegisterScreen}/>   */}
+      <Stack.Screen name="OnboardingScreen" component={OnboardingScreen}/>
+      <Stack.Screen name="LoginRegisterScreen" component={LoginRegisterScreen}/>  
             <Stack.Screen name="Tabbar" component={Tabbar}/>
-            {/* <Stack.Screen name="Chatscreen2" component={Chatscreen2}/> */}
+            <Stack.Screen name="Chatscreen2" component={Chatscreen2}/>
       </Stack.Navigator>
       </Provider>
     </NavigationContainer>
