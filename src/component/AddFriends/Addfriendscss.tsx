ca44f62c46d9e124f333eca3133a8e7fc30bf9c3
Nizam <ramazanov570633@gmail.com>
2023-08-05 21:32:10 +0400

    End Ui1

--- src/component/AddFriends/Addfriendscss.tsx
+++ src/component/AddFriends/Addfriendscss.tsx
@@ -1,32 +1,32 @@
-import { StyleSheet } from 'react-native'
-export default StyleSheet.create({
-    touchview: {
-    },
-    icon: {
-        color: '#771F98'
-    },
-    touc: {
-        alignSelf: 'center',
-        marginVertical: '90%',
-        borderWidth: 2,
-        borderColor: '#771F98',
-        borderRadius: 90,
-        padding: '10%'
-    },
-    touc1: {
-        alignSelf: 'center',
-        marginVertical: '50%',
-        borderWidth: 2,
-        borderColor: '#771F98',
-        borderRadius: 90,
-        padding: '10%',
-        margin:'7%'
+// import { StyleSheet } from 'react-native'
+// export default StyleSheet.create({
+//     touchview: {
+//     },
+//     icon: {
+//         color: '#771F98'
+//     },
+//     touc: {
+//         alignSelf: 'center',
+//         marginVertical: '90%',
+//         borderWidth: 2,
+//         borderColor: '#771F98',
+//         borderRadius: 90,
+//         padding: '10%'
+//     },
+//     touc1: {
+//         alignSelf: 'center',
+//         marginVertical: '50%',
+//         borderWidth: 2,
+//         borderColor: '#771F98',
+//         borderRadius: 90,
+//         padding: '10%',
+//         margin:'7%'
 
-    },
-    toucview: {
-        flexDirection: 'row',
-        position: 'absolute',
-        zIndex: 100
-    }
+//     },
+//     toucview: {
+//         flexDirection: 'row',
+//         position: 'absolute',
+//         zIndex: 100
+//     }
 
-})
+// })
